c5a063b530218eb7a41621915d68df36582d4c76
alexmetal87 <alessandro.ricci@exmachina.it>
2015-04-07 12:33:56 +0200

    d
    dfgdfg

---
+++ src/ios/ttsPlug_old2.m
@@ -0,0 +1,33 @@
+//
+//  ttsPlug.m
+//  tts
+//
+//  Created by Alex on 27/11/14.
+//
+//
+
+ #import <Cordova/CDV.h>
+ #import "ttsPlug.h"
+ #import <AVFoundation/AVFoundation.h>
+
+
+@implementation CustomTTS
+
+-(void)start:(CDVInvokedUrlCommand*)command {
+   
+    [self.commandDelegate runInBackground:^{
+       
+    NSString* myarg = [command.arguments objectAtIndex:1];
+        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:myarg];
+        [utterance setRate:0.25f];
+        AVSpeechSynthesizer *synth = [[AVSpeechSynthesizer alloc] init];
+        [synth speakUtterance:utterance];
+
+           
+    
+    }];
+    
+    
+}
+
+@end
