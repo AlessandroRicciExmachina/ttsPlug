15b6701c025b9b183f500ba3b24965eb7420db84
alexmetal87 <alessandro.ricci@exmachina.it>
2015-04-07 10:51:32 +0200

    fsd
    fsdf

--- src/ios/ttsPlug.m
+++ src/ios/ttsPlug.m
@@ -18,7 +18,7 @@
     [self.commandDelegate runInBackground:^{
        
     NSString* myarg = [command.arguments objectAtIndex:1];
-        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:@myarg];
+        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:myarg];
         [utterance setRate:0.25f];
         AVSpeechSynthesizer *synth = [[AVSpeechSynthesizer alloc] init];
         [synth speakUtterance:utterance];
