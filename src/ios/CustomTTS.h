9dbd2c8960ff9f12113d963c8442adee3db8fe25
alexmetal87 <alessandro.ricci@exmachina.it>
2015-04-08 18:08:35 +0200

    Update CustomTTS.h

--- src/ios/CustomTTS.h
+++ src/ios/CustomTTS.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <Cordova/CDVPlugin.h>
 @interface  CustomTTS : CDVPlugin
-
+@property (nonatomic, strong) AVSpeechSynthesizer* talker;
 -(void)start:(CDVInvokedUrlCommand*)command;
 
 @end
