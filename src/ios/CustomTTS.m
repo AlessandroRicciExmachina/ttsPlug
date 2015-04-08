147b95a1f95548b92f3e6b6d48ab0a53b3329cd4
alexmetal87 <alessandro.ricci@exmachina.it>
2015-04-08 16:51:36 +0200

    Update CustomTTS.m

--- src/ios/CustomTTS.m
+++ src/ios/CustomTTS.m
@@ -26,8 +26,8 @@
         [utterance setRate:0.25f];
         AVSpeechSynthesizer *synth = [[AVSpeechSynthesizer alloc] init];
         [synth speakUtterance:utterance];
-
-           
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     
     }];
     
